ea8a274cf3cd580b1fd522376899af2e22ce8d8e
anmol-23-singh <singhsher456a@gmail.com>
2025-07-30 11:30:08 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId =121212
+let accountemail ="singhsher345@gmail.com"
+
+// in modern period we use [ let and const ] only 
+
+var accountPassword = "hdebwe_ndcn"
+accountState = "jaipur"
+
+console.log(accountId);
+console.table([accountId, accountemail, accountPassword, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,10 @@
+"use strict"; 
+// this says we will going to practice new syntax and new modification of modern javascript 
+// without anhilating old javascript
+
+//     alert( 3 + 3 )      // it does not give output becoz it is node js. not browser that have engine 
+
+console.log( 3+3 )
+console.log(" Anmol ")
+
+// dont try to make them in single line , it will affect code redability
